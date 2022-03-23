db1cca2a0a4b070abeecc1be01a89127f3d7bbe6
Thomas Harvey <thomas@jigsaw.xyz>
2022-03-23 09:30:07 -0400

    adding support for additional query arguments in createReceiverRedirect

--- index.js
+++ index.js
@@ -4,16 +4,36 @@
 
   const _Palla = {};
 
+  // Utils
+
+  const mergeCallbackQuery = function (queryString, query) {
+    return queryString.split('&').reduce((acc, value) => {
+      const [key, val] = value.split('=');
+      if (!acc[key]) acc[key] = val;
+      return acc;
+    }, { ...query });
+  };
+
+  const objectToQueryString = obj => Object.entries(obj).map(
+    ([key, val]) => `${key}=${encodeURIComponent(val)}`
+  ).join('&');
+
   _Palla.createReceiverRedirect = function ({
     flowUrl,
     successUrl,
     errorUrl,
     accessToken,
+    ...query,
   }) {
-    const token = encodeURIComponent(accessToken);
-    const success = encodeURIComponent(successUrl);
-    const error = encodeURIComponent(errorUrl);
-    return flowUrl + '?token=' + token + '&success=' + success + '&error=' + error;
+    const url = new URL(flowUrl);
+    return url.origin + '?' + objectToQueryString(
+      mergeCallbackQuery(url.search.substring(1), {
+        ...query,
+        token: accessToken,
+        success: successUrl,
+        error: errorUrl,
+      })
+    );
   };
 
   window.Palla = _Palla;
